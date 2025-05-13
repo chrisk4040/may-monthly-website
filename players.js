34c971605d0e9d8746779c87339c709ed6e36849
chrisk4040 <526ckozeniesky@frhsd.com>
2025-05-13 09:30:36 -0400

    setup

---
+++ players.js
@@ -0,0 +1,38 @@
+const players = [
+    {
+        firstName: 'christopher',
+        lastName: 'Wilson',
+        position: 'ss',
+        age: 31,
+        photo: 'https://picsum.photos/200/300'
+    },
+    {
+        firstName: 'christopher',
+        lastName: 'Wilson',
+        position: 'ss',
+        age: 31,
+        photo: 'https://picsum.photos/200/300'
+    },
+    {
+        firstName: 'christopher',
+        lastName: 'Wilson',
+        position: 'ss',
+        age: 31,
+        photo: 'https://picsum.photos/200/300'
+    },
+    {
+        firstName: 'christopher',
+        lastName: 'Wilson',
+        position: 'ss',
+        age: 31,
+        photo: 'https://picsum.photos/200/300'
+    },
+    {
+        firstName: 'christopher',
+        lastName: 'Wilson',
+        position: 'ss',
+        age: 31,
+        photo: 'https://picsum.photos/200/300'
+    },
+]
+
