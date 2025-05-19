2050ed4d0cbe858565bac94cf785e8533cb67b31
chrisk4040 <526ckozeniesky@frhsd.com>
2025-05-19 10:39:49 -0400

    did some styling fixed the array and the function that creates the staff cards

--- roster.js
+++ roster.js
@@ -2,8 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
 const grid = document.getElementById('rosterGrid')
 const render = list => {
-    grid.innerHTml = '',
- 
+    grid.innerHTML = '',
+
     list.forEach(p  => {
         const col = document.createElement('div')
 
@@ -11,10 +11,10 @@ const render = list => {
 
         col.innerHTML =  `
         
-        <div class="card h-100 shadow-sm">
-        <img src= "${p.photo}" class="card-img-top" alt="${p.firstName} ${p.lastName}">
+        <div class="card h-100 shadow-sm" id="card">
+        <img src= "${p.picture}" class="card-img-top" alt="${p.name}">
         <div class= "card-body text-center">
-        <h5 class="card-title mb-1"> ${p.firstName} ${p.lastName}</h5>
+        <h5 class="card-title mb-1"> ${p.name}</h5>
         <div class="badge badge-position badge-position badge-pos-${p.position}">
                 ${p.position} </div>
         <p class= "small text-muted mb-0"> 
@@ -24,12 +24,43 @@ const render = list => {
             </div>
         `
        
-        
         grid.appendChild(col)
     })
 }
 
-
-
 render(players)
 }) 
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+const Grid = document.getElementById('staffGrid')
+const render = list => {
+    Grid.innerHTML = '',
+
+    list.forEach(s  => {
+        const col = document.createElement('div')
+
+        col.className = 'col-6 col-lg-2'
+
+        col.innerHTML =  `
+        
+        <div class="card h-100 shadow-sm" id="card">
+        <img src= "${s.picture}" class="card-img-top" alt="${s.name}">
+        <div class= "card-body text-center">
+        <h5 class="card-title mb-1"> ${s.name}</h5>
+        <div class="badge badge-position badge-position badge-pos-${s.position}">
+                ${s.position} </div>
+        <p class= "small text-muted mb-0"> 
+            age ${s.age}
+            </p>
+            </div>
+            </div>
+        `
+       
+        Grid.appendChild(col)
+    })
+}
+
+render(japanVolleyballCoachesAndTrainers)
+})
